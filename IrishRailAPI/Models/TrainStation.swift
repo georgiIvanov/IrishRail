5b3e10312706d316da10964fd749dab985e3b4c7
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2021-01-16 12:45:32 +0200

    TrainStation model.

---
+++ IrishRailAPI/Models/TrainStation.swift
@@ -0,0 +1,16 @@
+//
+//  TrainStation.swift
+//  IrishRailAPI
+//
+//  Created by Voro on 15.01.21.
+//
+
+import Foundation
+
+struct TrainStation {
+    let name: String
+    let alias: String?
+    let latitude: Double
+    let longitude: Double
+    let code: String
+}
