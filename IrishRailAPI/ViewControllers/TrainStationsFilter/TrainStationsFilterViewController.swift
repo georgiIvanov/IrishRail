765b323e89e77a66cfa11b9019e27095a0e9c735
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2021-01-16 16:58:50 +0200

    Declare TrainStationsFilterViewController, segue trigger for it.

---
+++ IrishRailAPI/ViewControllers/TrainStationsFilter/TrainStationsFilterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TrainStationsFilterViewController.swift
+//  IrishRailAPI
+//
+//  Created by Voro on 16.01.21.
+//
+
+import Foundation
+import UIKit
+
+class TrainStationsFilterViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- IrishRailAPI/ViewControllers/TrainsSearch/TrainsSearchViewController.swift
+++ IrishRailAPI/ViewControllers/TrainsSearch/TrainsSearchViewController.swift
@@ -10,4 +10,29 @@ import UIKit
 
 class TrainsSearchViewController: UIViewController {
     
+    @IBOutlet weak var fromStationView: TrainStopView!
+    @IBOutlet weak var toStationView: TrainStopView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    func setupUI() {
+        fromStationView.onViewTap = { [weak self] (view)  in
+            self?.performSegue(withIdentifier: "trainStationsFilterSegue", sender: view)
+        }
+        
+        toStationView.onViewTap = { [weak self] (view)  in
+            self?.performSegue(withIdentifier: "trainStationsFilterSegue", sender: view)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let filterVc = segue.destination as? TrainStationsFilterViewController,
+           let view = sender as? TrainStopView {
+            print(view.direction)
+            // TODO: setup filter bindings
+        }
+    }
 }
