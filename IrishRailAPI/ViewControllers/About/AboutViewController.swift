2dbd64f3f16b95691fac0620f374ae386e3e6c6a
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2021-01-20 17:29:19 +0200

    Add AboutViewController.

---
+++ IrishRailAPI/ViewControllers/About/AboutViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AboutViewController.swift
+//  IrishRailAPI
+//
+//  Created by Voro on 20.01.21.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class AboutViewController: UIViewController {
+
+    @IBOutlet var closeButton: UIButton!
+    
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindUI()
+    }
+    
+    func bindUI() {
+        closeButton.rx.tap.subscribe(onNext: { [weak self] _ in
+            self?.dismiss(animated: true, completion: nil)
+        }).disposed(by: disposeBag)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
